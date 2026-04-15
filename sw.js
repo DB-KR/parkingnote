7c705063ede602705ee06a4c020a4a2f7ea7fa41
DB-KR <guardm777@gmail.com>
2026-04-15 11:09:04 +0900

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,13 @@
+self.addEventListener('install', (e) => {
+  self.skipWaiting();
+});
+
+self.addEventListener('notificationclick', (event) => {
+  event.notification.close();
+  event.waitUntil(
+    clients.matchAll({ type: 'window' }).then((clientList) => {
+      if (clientList.length > 0) return clientList[0].focus();
+      return clients.openWindow('./index.html');
+    })
+  );
+});
